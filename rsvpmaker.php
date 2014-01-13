f604dab8aafc95469a578e3be122fcf43c7a336c
davidfcarr <davidfcarr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-13 03:02:42 +0000

    tagging 3.0.2
    git-svn-id: https://plugins.svn.wordpress.org/rsvpmaker/tags/3.0.2@837453 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rsvpmaker.php
+++ rsvpmaker.php
@@ -3,9 +3,9 @@
 /*
 Plugin Name: RSVPMaker
 Plugin URI: http://www.rsvpmaker.com
-Description: Schedule events and solicit RSVPs. The editor is built around the custom post types feature introduced in WP 3.0, so you get all your familiar post editing tools with a few extra options for setting dates and RSVP options. PayPal payments can be added with a little extra configuration. <a href="options-general.php?page=rsvpmaker-admin.php">Options</a> / <a href="edit.php?post_type=rsvpmaker&page=rsvpmaker_doc">Shortcode documentation</a>. Note that if you delete RSVPMaker from the control panel, all associated data will be deleted automatically including contact info of RSVP respondents. To delete data more selectively, use the <a href="/wp-content/plugins/rsvpmaker/cleanup.php">cleanup utility</a> in the plugin directory.
+Description: Schedule events and solicit RSVPs. Events are implemented as custom post types, so you get all your familiar post editing tools with extra options for setting dates and RSVP options. PayPal payments can be added with a little extra configuration. Recurring events can be tracked according to a schedule such as "First Monday" or "Every Friday" at a specified time, and the software will calculate future dates according to that schedule and let you track them together. <a href="options-general.php?page=rsvpmaker-admin.php">Options</a> / <a href="edit.php?post_type=rsvpmaker&page=rsvpmaker_doc">Shortcode documentation</a>. Note that if you delete RSVPMaker from the control panel, all associated data will be deleted automatically including contact info of RSVP respondents. To delete data more selectively, use the <a href="/wp-content/plugins/rsvpmaker/cleanup.php">cleanup utility</a> in the plugin directory.
 Author: David F. Carr
-Version: 2.5.3.1
+Version: 3.0.2
 Author URI: http://www.carrcommunications.com
 */
 
@@ -24,11 +24,24 @@ $rsvp_options = get_option('RSVPMAKER_Options');
 
 //defaults
 if(!isset($rsvp_options["menu_security"]))
-	$rsvp_options["menu_security"] = 'manage_options';
+	$rsvp_options["menu_security"] = 'manage_options'; // rsvp report
+if(!isset($rsvp_options["rsvpmaker_template"]))
+	$rsvp_options["rsvpmaker_template"] = 'publish_rsvpmakers';
+if(!isset($rsvp_options["recurring_event"]))
+	$rsvp_options["recurring_event"] = 'publish_rsvpmakers';
+if(!isset($rsvp_options["multiple_events"]))
+	$rsvp_options["multiple_events"] = 'publish_rsvpmakers';
+if(!isset($rsvp_options["documentation"]))
+	$rsvp_options["documentation"] = 'edit_rsvpmakers';
+
 if(!isset($rsvp_options["rsvp_to"]))
 	$rsvp_options["rsvp_to"] = get_bloginfo('admin_email');
 if(!isset($rsvp_options["rsvp_confirm"]))
 	$rsvp_options["rsvp_confirm"] = __('Thank you!','rsvpmaker');
+if(!isset($rsvp_options["rsvp_count"]))
+	$rsvp_options["rsvp_count"] = 1;
+if(!isset($rsvp_options["rsvp_yesno"]))
+	$rsvp_options["rsvp_yesno"] = 1;
 if(!isset($rsvp_options['rsvplink']))
 	$rsvp_options['rsvplink'] = '<p><a style="width: 8em; display: block; border: medium inset #FF0000; text-align: center; padding: 3px; background-color: #0000FF; color: #FFFFFF; font-weight: bolder; text-decoration: none;" class="rsvplink" href="%s?e=*|EMAIL|*#rsvpnow">'. __('RSVP Now!','rsvpmaker').'</a></p>';
 if(!isset($rsvp_options['defaulthour']))
@@ -43,52 +56,40 @@ if(!isset($rsvp_options["short_date"]))
 if(!isset($rsvp_options["time_format"]))
 	$rsvp_options["time_format"] = 'g:i A';
 
-if(!isset($rsvp_options["rsvp_form"]))
-	$rsvp_options["rsvp_form"] = '
-        <table border="0" cellspacing="0" cellpadding="0"> 
-          <tr> 
-            <td>'. __('First Name','rsvpmaker').':</td> 
-            <td> 
-              [rsvpfield textfield="first"]
-            </td> 
-          </tr> 
-          <tr> 
-            <td>'. __('Last Name','rsvpmaker').':</td> 
-            <td> 
-              [rsvpfield textfield="last"]
-            </td> 
-          </tr> 
-          <tr> 
-            <td width="100">'.__('Email','rsvpmaker').':</td>
-            <td>[rsvpfield textfield="email"]</td> 
-          </tr> 
+if(!isset($rsvp_options["rsvp_form"]) || isset($_GET["reset_form"]))
+	$rsvp_options["rsvp_form"] = '<table border="0" cellspacing="0" cellpadding="0" width="100%"> 
+<tr> 
+<td>'. __('First Name','rsvpmaker').':</td><td>[rsvpfield textfield="first" required="1"]</td> 
+</tr> 
+<tr> 
+<td>'. __('Last Name','rsvpmaker').':</td><td>[rsvpfield textfield="last" required="1"]</td> 
+</tr> 
+<tr> 
+<td width="100">'.__('Email','rsvpmaker').':</td><td>[rsvpfield textfield="email" required="1"]</td> 
+</tr>
 </table>
-
-<!-- by default, this displays the phone # field or a message saying this is already on file can also be customized to request more extensive contact info -->
-
 [rsvpprofiletable show_if_empty="phone"]
-<table border="0" cellspacing="0" cellpadding="0"> 
+<table border="0" cellspacing="0" cellpadding="0" width="100%">
 <tr> 
 <td width="100">'.__('Phone','rsvpmaker').':</td> 
 <td>[rsvpfield textfield="phone" size="20"]</td> 
 </tr> 
 <tr> 
 <td>'.__('Phone Type','rsvpmaker').':</td>
-<td>[rsvpfield selectfield="phonetype" options="'.__('Work Phone','rsvpmaker').','.__('Mobile Phone','rsvpmaker').','.__('Home Phone','rsvpmaker').'</option> 
-"]</td> 
-</tr> 
+<td>[rsvpfield selectfield="phonetype" options="'.__('Work Phone','rsvpmaker').','.__('Mobile Phone','rsvpmaker').','.__('Home Phone','rsvpmaker').'"]</td> 
+</tr>
 </table>
 [/rsvpprofiletable]
-
-<!-- end of profile section-->      
-
-[rsvpguests]
-        
+[rsvpguests]      
 <p>'. __('Note','rsvpmaker').':<br /> 
-<textarea name="note" cols="60" rows="2" id="note"></textarea> 
+<textarea name="note" cols="60" rows="2" id="note">[rsvpnote]</textarea> 
 </p>
 ';
+if(!isset($rsvp_options["smtp"]) )
+	$rsvp_options["smtp"] = '';
 
+if(isset($_GET["reset_form"]))
+	update_option('RSVPMAKER_Options',$rsvp_options);
 
 if(!isset($rsvp_options["paypal_currency"]))
 	$rsvp_options["paypal_currency"] = 'USD';
@@ -98,7 +99,7 @@ if(!isset($rsvp_options["currency_thousands"]))
 	$rsvp_options["currency_thousands"] = ',';
 
 if(file_exists(WP_PLUGIN_DIR."/rsvpmaker-custom.php") )
-	include WP_PLUGIN_DIR."/rsvpmaker-custom.php";
+	include_once WP_PLUGIN_DIR."/rsvpmaker-custom.php";
 
 include WP_PLUGIN_DIR."/rsvpmaker/rsvpmaker-admin.php";
 include WP_PLUGIN_DIR."/rsvpmaker/rsvpmaker-display.php";
@@ -107,15 +108,18 @@ include WP_PLUGIN_DIR."/rsvpmaker/rsvpmaker-plugabble.php";
 add_action( 'init', 'rsvpmaker_create_post_type' );
 
 function rsvpmaker_create_post_type() {
+global $rsvp_options;
+$menu_label = (isset($rsvp_options["menu_label"])) ? $rsvp_options["menu_label"] : __("RSVP Events",'rsvpmaker');
+$supports = ( isset($rsvp_options["rsvpmaker_supports"]) ) ? $rsvp_options["rsvpmaker_supports"] : array('title','editor','author','excerpt','custom-fields');
 
   register_post_type( 'rsvpmaker',
     array(
       'labels' => array(
-        'name' => __( 'RSVP Events' ),
-        'add_new_item' => __( 'Add New Event' ),
-        'edit_item' => __( 'Edit Event' ),
-        'new_item' => __( 'Events' ),
-        'singular_name' => __( 'Event' )
+        'name' => $menu_label,
+        'add_new_item' => __( 'Add New Event','rsvpmaker' ),
+        'edit_item' => __( 'Edit Event','rsvpmaker' ),
+        'new_item' => __( 'Events','rsvpmaker' ),
+        'singular_name' => __( 'Event','rsvpmaker' )
       ),
     'menu_icon' => plugins_url('/calendar.png',__FILE__),
 	'public' => true,
@@ -123,27 +127,29 @@ function rsvpmaker_create_post_type() {
     'show_ui' => true, 
     'query_var' => true,
     'rewrite' => array( 'slug' => 'rsvpmaker','with_front' => FALSE), 
-    'capability_type' => 'post',
+    'capability_type' => 'rsvpmaker',
+    'map_meta_cap' => true,
+    'has_archive' => true,
     'hierarchical' => false,
     'menu_position' => 5,
-    'supports' => array('title','editor','author','excerpt','custom-fields'),
+    'supports' => $supports,
 	'taxonomies' => array('rsvpmaker-type','post_tag')
     )
   );
 
   // Add new taxonomy, make it hierarchical (like categories)
   $labels = array(
-    'name' => _x( 'Event Types', 'taxonomy general name' ),
-    'singular_name' => _x( 'Event Type', 'taxonomy singular name' ),
-    'search_items' =>  __( 'Search Event Types' ),
-    'all_items' => __( 'All Event Types' ),
-    'parent_item' => __( 'Parent Event Type' ),
-    'parent_item_colon' => __( 'Parent Event Type:' ),
-    'edit_item' => __( 'Edit Event Type' ), 
-    'update_item' => __( 'Update Event Type' ),
-    'add_new_item' => __( 'Add New Event Type' ),
-    'new_item_name' => __( 'New Event Type' ),
-    'menu_name' => __( 'Event Type' ),
+    'name' => _x( 'Event Types', 'taxonomy general name', 'rsvpmaker' ),
+    'singular_name' => _x( 'Event Type', 'taxonomy singular name', 'rsvpmaker' ),
+    'search_items' =>  __( 'Search Event Types','rsvpmaker' ),
+    'all_items' => __( 'All Event Types','rsvpmaker' ),
+    'parent_item' => __( 'Parent Event Type','rsvpmaker' ),
+    'parent_item_colon' => __( 'Parent Event Type:','rsvpmaker' ),
+    'edit_item' => __( 'Edit Event Type','rsvpmaker' ), 
+    'update_item' => __( 'Update Event Type','rsvpmaker' ),
+    'add_new_item' => __( 'Add New Event Type','rsvpmaker' ),
+    'new_item_name' => __( 'New Event Type','rsvpmaker' ),
+    'menu_name' => __( 'Event Type','rsvpmaker' ),
   ); 	
 
   register_taxonomy('rsvpmaker-type',array('rsvpmaker'), array(
@@ -158,6 +164,11 @@ function rsvpmaker_create_post_type() {
 global $rsvp_options;
 if(isset($rsvp_options["flush"]) && $rsvp_options["flush"])
 	flush_rewrite_rules();
+
+// if there is a logged in user, set editing roles
+global $current_user;
+if( isset($current_user) )
+	rsvpmaker_roles();
 
 }
 
@@ -194,6 +205,7 @@ $sql = "CREATE TABLE `".$wpdb->prefix."rsvpmaker` (
   `guestof` varchar(255)   CHARACTER SET utf8 COLLATE utf8_general_ci  default NULL,
   `note` text   CHARACTER SET  utf8 COLLATE utf8_general_ci NOT NULL,
   `participants` INT NOT NULL DEFAULT '0',
+  `user_id` INT NOT NULL DEFAULT '0',
   `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;";
@@ -209,7 +221,7 @@ $sql = "CREATE TABLE `".$wpdb->prefix."rsvp_volunteer_time` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;";
 dbDelta($sql);
 
-$rsvp_options["dbversion"] = 4;
+$rsvp_options["dbversion"] = 5;
 update_option('RSVPMAKER_Options',$rsvp_options);
 
 global $wpdb;
@@ -244,5 +256,23 @@ if($rsvp_options["dbversion"] < 4)
 	$wpdb->query("ALTER TABLE `wp_rsvpmaker` CHANGE `details` `details` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ");	
 	$wpdb->query("ALTER TABLE `wp_rsvpmaker` CHANGE `note` `note` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ");
 	}
+if($rsvp_options["dbversion"] < 5)
+	cpevent_activate();
+
+
+function rsvpmaker_template_order( $templates='' )
+{
+global $post;
+if($post->post_type != 'rsvpmaker')
+    return $templates;
+
+if(!is_array($templates) && strpos($templates, 'single-rsvpmaker' ) )
+   	 return $templates;
+	 
+   	$template = locate_template(array("single-rsvpmaker.php","page.php","single.php","index.php"),false);
+
+return $templates;
+}
+add_filter( 'single_template', 'rsvpmaker_template_order' );
 
 ?>
